e931f46f69196fbbc4d70ab89f8c25a3e4ff117f
Adam Tomczyk <atomczyk>
2023-01-12 20:17:47 +0100

    init

--- scrape_data.py
+++ scrape_data.py
@@ -1,48 +1,30 @@
-import json
-import thingspeak
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-from datetime import datetime
-from bokeh.plotting import figure, show
-from bokeh.models import DatetimeTickFormatter
-import numpy as np
-
-from bokeh.layouts import column, row
-from bokeh.models import CustomJS, Slider
-from bokeh.plotting import ColumnDataSource, figure, show, curdoc
-from bokeh.client import push_session, pull_session
+from datetime import datetime, timedelta
+from bokeh.plotting import figure, ColumnDataSource, figure, curdoc
+from bokeh.models import DatetimeTickFormatter, Slider, HoverTool, Slider, DatePicker, MultiChoice
+from bokeh.layouts import row
+from bokeh.client import push_session
+import requests
 
 
-ch = thingspeak.Channel(202842)
-data = json.loads(ch.get({'results': 5000}))
+def get_data_by_day_number(date):
+    start_data = str(date)
+    end_data = str(date + timedelta(days=1))
+    print(start_data, end_data)
+    response = requests.get('https://api.thingspeak.com/channels/202842/feeds.json?start={start_data}&end={end_data}'.format(start_data=start_data, end_data=end_data))
+    data = response.json()
+    data = list(map(lambda x: [x['created_at'], x['field1'], x['field2'], x['field3'], x['field4'], x['field5'], x['field6'], x['field7'], x['field8']], data['feeds']))
 
-data_structure = {
-    "created_at": "2016-12-13T13:54:29Z",
-    "description": "Monitoring parametr\u00f3w mikroklimatu wewn\u0105trz pomieszczenia (temperatura, wilgotno\u015b\u0107, ci\u015bnienie, nat\u0119\u017cenie \u015bwiat\u0142a, ruch) ",
-    "field1": "Temperatura (DHT-22) [\u00b0C]",
-    "field2": "Wilgotno\u015b\u0107 wzgl\u0119dna (DHT-22) [%]",
-    "field3": "Nat\u0119\u017cenie \u015bwiat\u0142a (BH-1750) [lx]",
-    "field4": "Ci\u015bnienie atm. (BMP-180) [hPa]",
-    "field5": "Temp. grzejnika (DS18B20) [\u00b0C]",
-    "field6": "Temperatura (DS18B20) [\u00b0C]",
-    "field7": "Ruch (PIR)",
-    "field8": "Temperatura  (BMP-180) [\u00b0C]",
-    "id": 202842,
-    "last_entry_id": 2749954,
-    "latitude": "52.403363",
-    "longitude": "16.949372",
-    "name": "L.2.7.14BT",
-    "updated_at": "2018-09-04T07:49:05Z"
-  }
+    plot_data = {}
+    labels = ['temperature'] #, 'humidity', 'light', 'pressure', 'radiator_temperature', 'temperature_DS18B20', 'movement', 'temperature_BMP180']
+    for [index, label] in enumerate(labels):
+        temp_data = list(filter(lambda x: x[1 + index] is not None, data.copy()))
 
-data = list(map(lambda x: [x['created_at'], x['field1'], x['field2'], x['field3'], x['field4'], x['field5'], x['field6'], x['field7'], x['field8']], data['feeds']))
+        plot_data[label] = {
+            'dates': list(map(lambda x: [datetime.strptime(x[0], '%Y-%m-%dT%H:%M:%SZ')], temp_data.copy())),
+            'values': list(map(lambda x: [float(x[1 + index])], temp_data.copy()))
+        }
+    return plot_data['temperature']
 
-plot_data = {}
-labels = ['temperature', 'humidity', 'light', 'pressure', 'radiator_temperature', 'temperature_DS18B20', 'movement', 'temperature_BMP180']
-for [index, label] in enumerate(labels):
-    temp_data = list(filter(lambda x: x[1 + index] is not None, data.copy()))
-    plot_data[label + '_dates'] = list(map(lambda x: [datetime.strptime(x[0], '%Y-%m-%dT%H:%M:%SZ')], temp_data.copy()))
-    plot_data[label + '_values'] = list(map(lambda x: [float(x[1 + index])], temp_data.copy()))
 
 plot = figure(title="Simple line example", x_axis_label="x", x_axis_type="datetime", y_axis_label="y")
 
@@ -50,30 +32,31 @@ plot.xaxis.formatter=DatetimeTickFormatter(
     hours=["%d.%m.%y %H:%M"],
 )
 
-source_x = plot_data['temperature_dates']
-source_y = plot_data['temperature_values']
+source = ColumnDataSource(data = get_data_by_day_number(datetime.now().replace(hour=0).replace(minute=0).replace(second=0).replace(microsecond=0)))
 
-# plot.line('x', 'y', source=source, line_width=3, line_alpha=0.6)
-plot.line(source_x, source_y, legend_label="Temp.", line_width=2)
+plot.line(x='dates', y='values', source=source, legend_label="Temp.", line_width=2)
 
-slider = Slider(start=0.1, end=10, value=1, step=.1, title="Amplitude")
-
+#slider
+slider = Slider(start=1, end=31, value=11, step=1, title="Day")
 def slider_callback(attr, old, new):
-    s = slider.value
-    # x = r.source.data['x']
-    # y = []
-
-    # for value in x:
-    #     y.append((s * value) + i)
-
-    # r.source.data['y'] = y
-    # print(y)
-
+    print(new)
+    source.data=get_data_by_day_number(new)
 slider.on_change('value', slider_callback)
+
+#hover
+hover = HoverTool(tooltips=[('date', '@dates{%d.%m.%y %H:%M}'), ("temperatura", "@values C")],
+          formatters={'@dates': 'datetime'})
+plot.tools.append(hover)
+
+#datepicker
+def date_picker_callback(attr, old, new):
+    source.data=get_data_by_day_number(datetime(int(new[0:4]), int(new[5:7]), int(new[8:10])))
+date_picker = DatePicker(title='Select date', value="2023-01-12", min_date="2022-01-12", max_date="2024-01-12")
+date_picker.on_change('value', date_picker_callback)
 
 layout = row(
     plot,
-    slider,
+    date_picker,
 )
 
 curdoc().add_root(layout)
